e5d4ed6fe533a10dc4e6ea7bbde96d55b5f6e73d
santoshkarpe <karpe4u@gmail.com>
2020-07-10 08:16:36 +0530

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserSetingFormComponent } from './user-seting-form/user-seting-form.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent,
     UserSetingFormComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
